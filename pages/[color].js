be00639597c2f2fd79d236c3ced6f3d683f4d50c
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-11-14 12:44:17 +0100

    ssr commit

--- pages/[color].js
+++ pages/[color].js
@@ -1,20 +1,28 @@
-import colors from '../data/colors.json'
+import colors from "../data/colors.json";
 
 export default function Color({ color }) {
-  return <div className='color-page' style={{ backgroundColor: color.hex }}>
-    <h1>{color.name}</h1>
-  </div>
+  return (
+    <div className="color-page" style={{ backgroundColor: color.hex }}>
+      <h1>{color.name}</h1>
+    </div>
+  );
 }
 
-export async function getStaticProps({ params }) {
-  const color = colors.find(color => color.name === params.color)
-  return { props: { color } }
+export async function getServerSideProps({ params }) {
+  const color = colors.find((color) => color.name === params.color);
+    return { props: { color } }
+
 }
 
-export async function getStaticPaths() {
-  const paths = colors.map(color => ({
-    params: { color: color.name }
-  }))
+// export async function getStaticProps({ params }) {
+//   const color = colors.find(color => color.name === params.color)
+//   return { props: { color } }
+// }
 
-  return { paths, fallback: false }
-}
+// export async function getStaticPaths() {
+//   const paths = colors.map(color => ({
+//     params: { color: color.name }
+//   }))
+
+//   return { paths, fallback: false }
+// }
